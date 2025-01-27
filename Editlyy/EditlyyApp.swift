b80bcaa6d5123728a7d353318aa88d5336269dd9
Mücahit Kökdemir <76880536+mucahitdev@users.noreply.github.com>
2025-01-27 22:09:28 +0300

    Initial Commit

---
+++ Editlyy/EditlyyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditlyyApp.swift
+//  Editlyy
+//
+//  Created by Mücahit Kökdemir NTT on 27.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct EditlyyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
